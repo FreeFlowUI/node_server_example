28d42f79d3a353ceb664fa491e5b88eb1c861d7c
Nabhya Sharma <nsharma7c@gmail.com>
2024-11-24 20:24:56 +0530

    Reformatted Imports

--- src/view/sample_page.ts
+++ src/view/sample_page.ts
@@ -1,28 +1,39 @@
-import {Container,} from "freeflow-ui";
-import { AppBar, Color, ListView, Row, Scaffold, SizedBox, Text } from "freeflow-ui";
-import { BoxDecoration, CrossAxisAlignment, MainAxisAlignment, EdgeInsets } from "freeflow-ui";
-import {TextStyle} from "freeflow-ui";
+import {
+  AppBar,
+  Color,
+  ListView,
+  Row,
+  Scaffold,
+  SizedBox,
+  Text,
+  BoxDecoration,
+  CrossAxisAlignment,
+  MainAxisAlignment,
+  EdgeInsets,
+  Widget,
+  TextStyle,
+  Container,
+} from "freeflow-ui";
 import { listViewItem } from "./component/listViewItem";
-import {Widget} from "freeflow-ui";
 
-const listOfThings = [1,2, 3,4, 5];
+const listOfThings = [1, 2, 3, 4, 5];
 let children: Widget[] = [];
 
-listOfThings.forEach((data)=>children.push(listViewItem(`${data}`)));
+listOfThings.forEach((data) => children.push(listViewItem(`${data}`)));
 
 const sample = new Scaffold({
-    appBar: new AppBar({
-        child: new Text({
-            value: "The Bird Blog",
-            textStyle: new TextStyle({
-                fontSize: 24,
-                fontWeight: 1
-            }),
-        }),
+  appBar: new AppBar({
+    child: new Text({
+      value: "The Bird Blog",
+      textStyle: new TextStyle({
+        fontSize: 24,
+        fontWeight: 1,
+      }),
     }),
-    body: new ListView({
-        children
-    })
+  }),
+  body: new ListView({
+    children,
+  }),
 });
 
 export default sample;
